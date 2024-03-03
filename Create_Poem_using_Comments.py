3799ad57b9709752c7c98b833d584f40a621385a
nikitaan1 <ancevskinikita@gmail.com>
2024-03-03 12:07:50 -0500

    add create poem using comments

---
+++ Create_Poem_using_Comments.py
@@ -0,0 +1,11 @@
+# Python, oh Python, so clear and so neat
+# With every new challenge, you're hard to beat.
+# Your syntax dances, a rhythmic feat,
+# In loops and functions, you're ever complete,
+# A language of power, from terminal to sheet.
+
+
+
+
+
+

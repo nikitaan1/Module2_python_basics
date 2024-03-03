6d4a90fef800bcfaf1fbddb9660e4e13fda0fa3b
nikitaan1 <ancevskinikita@gmail.com>
2024-03-03 12:14:20 -0500

    multi line poem

---
+++ Multi-line_Poem.py
@@ -0,0 +1,7 @@
+'''
+Python, in the realm of code you shine,
+With simplicity and grace, you're truly divine.
+Your libraries vast, like stars in the sky,
+Guiding programmers as they reach new highs.
+
+'''

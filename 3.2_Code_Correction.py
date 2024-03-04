d40e0419fc4cc82c5264e4c6563e64d8c8d273cf
nikitaan1 <ancevskinikita@gmail.com>
2024-03-03 20:30:13 -0500

    adding 3.2 file

---
+++ 3.2_Code_Correction.py
@@ -0,0 +1,9 @@
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a), variable_a)
+print(type(variable_b), variable_b)
+print(type(variable_c), variable_c)
+print(type(variable_d), variable_d)

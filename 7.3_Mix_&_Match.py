add0e3a96ff934b4e542e22efd451fc8ead17db3
nikitaan1 <ancevskinikita@gmail.com>
2024-03-04 23:29:22 -0500

    add 7.3 file

---
+++ 7.3_Mix_&_Match.py
@@ -0,0 +1,24 @@
+
+Name = "George"
+annual_income = 90000
+average_annual_income = 60000
+debt = 45336
+
+
+# Calculate if the annual income is greater than the average
+
+if annual_income > average_annual_income:
+    print(Name + " has greater annual income than the average")
+
+else:
+    print(Name + " has greater annual income than the average")
+
+
+    # Calculate how many months the person needs to work to cover the debts. Round the number
+
+debt_free = debt // (annual_income / 12)
+print(debt_free)
+
+
+
+

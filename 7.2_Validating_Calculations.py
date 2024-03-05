059dee9cd7d985bc65e584f01f8a61df8a1ab05e
nikitaan1 <ancevskinikita@gmail.com>
2024-03-04 22:37:03 -0500

    adding 7.2 file

---
+++ 7.2_Validating_Calculations.py
@@ -0,0 +1,8 @@
+
+result1 = 4 + 5 * 5 + 6 / 10
+result2 = 4 + 5 * (5 + 6) / 10
+
+if result1 == result2:
+    print("The results match")
+if result1 != result2:
+    print("The results don't match")

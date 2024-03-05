9a3a8455cc0daf0e8d8dd78041f3e8dee5e358c6
nikitaan1 <ancevskinikita@gmail.com>
2024-03-04 22:21:24 -0500

    adding file 7.1

---
+++ 7.1_Simple_Logic_Puzzles.py
@@ -0,0 +1,22 @@
+a = True
+b = False
+
+
+if a == True and b == False:
+    print(True)
+
+else:
+    print(False)
+
+
+if a == True or b == False:
+    print(True)
+
+else:
+    print(False)
+
+if not a == True and b == False:
+    print(True)
+
+else:
+    print(False)
